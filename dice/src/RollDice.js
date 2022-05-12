2200922072576cd5f9bcc56aef13c33227875544
HaruYoshiyuki <69156750+HaruYoshiyuki@users.noreply.github.com>
2022-05-12 12:48:54 +0700

    The dice project

---
+++ dice/src/RollDice.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import Dice from './Die';
+import './RollDice.css';
+
+class RollDice extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { die1:'one', die2:'two', rolling:false };
+        this.roll = this.roll.bind(this);
+    }
+    static defaultProps = {
+        sides: ['one', 'two', 'three', 'four', 'five', 'six']
+    };
+    roll() {
+        //pick 2 new rolls
+        const newDie1 = this.props.sides[
+            Math.floor(Math.random() * this.props.sides.length)
+        ];
+        const newDie2 = this.props.sides[
+            Math.floor(Math.random() * this.props.sides.length)
+        ];
+        //set state with new rolls
+        this.setState({ die1:newDie1, die2: newDie2, rolling: true });
+
+        //wait one second than set rolling to false
+        setTimeout(() => {
+            this.setState({rolling: false})
+        }, 1000);
+    }
+    render() {
+        return (
+            <div className='RollDice'>
+                <div className='dice-container'>
+                    <Dice face={this.state.die1} rolling={this.state.rolling}/>
+                    <Dice face={this.state.die2} rolling={this.state.rolling}/>
+                </div>
+                <button onClick={this.roll} disabled={this.state.rolling}>
+                    {this.state.rolling ? "Rolling..." : "Roll Dice!!"}</button>
+            </div>
+        )
+    } 
+}
+export default RollDice;

b88952b4e63f76427652ff3782e46d595cd82780
HaruYoshiyuki <69156750+HaruYoshiyuki@users.noreply.github.com>
2022-05-12 13:02:58 +0700

    React-projects

---
+++ pokemon/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+import Pokegame from './Pokegame';
+
+function App() {
+  return (
+    <div className="App">
+      <Pokedex />
+    </div>
+  );
+}
+
+export default App;
---
+++ todo-app/src/NewTodoForm.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import { v4 as uuidv4 } from 'uuid';
+import './NewTodoForm.css';
+
+class NewTodoForm extends Component {
+    constructor(props){
+        super(props);
+        this.state = {task:""};
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+    handleChange(evt) {
+        this.setState({
+            [evt.target.name]: evt.target.value
+        })
+    }
+    handleSubmit(evt) {
+        evt.preventDefault();
+        this.props.createTodo({...this.state, id: uuidv4(), completed: false});
+        this.setState({task:""});
+    }
+    render() {
+        return <form className='NewTodoForm' onSubmit={this.handleSubmit}>
+            <label htmlFor='task'>New Todo:   </label>
+            <input
+                className='putin' 
+                value={this.state.task} 
+                name='task' type='text' 
+                placeholder='New Todo' 
+                onChange={this.handleChange}></input>
+            <input type='submit' value='Add todo' className='submit'></input>
+        </form>
+    }
+}
+export default NewTodoForm
